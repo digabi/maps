a03911ced233281fad4c8ddbc09e58da5c03353c
Sami Saves <sami.saves@reaktor.com>
2020-03-13 15:19:39 +0200

    Export map creation so it can be used as a package

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,32 @@
-import * as leaflet from 'leaflet'
-import 'leaflet/dist/leaflet.css'
+import * as leaflet from "leaflet"
+import "leaflet/dist/leaflet.css"
 
-const map = leaflet.map('map-container').setView([0, 0], 1);
-leaflet.tileLayer('/fi/{z}/{x}/{y}.png', { minZoom: 0, maxZoom: 9, attribution: 'YTL' }).addTo(map)
+const createMap = ({
+  mapUrl = "/fi/{z}/{x}/{y}.png",
+  minZoom = 0,
+  maxZoom = 9,
+  errorTileUrl = "/error.png"
+}) => {
+  const map = leaflet.map("map-container").setView([0, 0], 1)
+
+  const layerOptions: leaflet.TileLayerOptions = {
+    minZoom,
+    maxZoom,
+    attribution: "YTL",
+    errorTileUrl
+  }
+
+  leaflet.tileLayer(mapUrl, layerOptions).addTo(map)
+}
+
+declare global {
+  interface Window {
+    cheatMap: any;
+  }
+}
+
+if (typeof module === "object" && module.exports) {
+  module.exports = { createMap }
+} else {
+  window.cheatMap = { createMap }
+}
