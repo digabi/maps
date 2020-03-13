4405fab55514153cd6d3e94a2733b48c438cc649
Sami Saves <sami.saves@reaktor.com>
2020-03-13 13:02:33 +0200

    Add map tiles

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,5 @@
-console.log('Hello World')
+import * as leaflet from 'leaflet'
+import 'leaflet/dist/leaflet.css'
+
+const map = leaflet.map('map-container').setView([0, 0], 1);
+leaflet.tileLayer('/fi/{z}/{x}/{y}.png', { minZoom: 0, maxZoom: 9, attribution: 'YTL' }).addTo(map)
