8c57c341d682cff408c6961d20ff51ae45b18f34
Sami Saves <sami.saves@reaktor.com>
2020-03-13 13:02:12 +0200

    Add style, css and file loaders to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,10 +11,22 @@ module.exports = {
         use: 'ts-loader',
         exclude: /node_modules/,
       },
+      {
+        test: /\.css$/,
+        use: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(png|jpe?g|gif)$/i,
+        use: [
+          {
+            loader: 'file-loader',
+          },
+        ],
+      },
     ],
   },
   resolve: {
-    extensions: [ '.tsx', '.ts', '.js' ],
+    extensions: ['.tsx', '.ts', '.js'],
   },
   output: {
     filename: 'bundle.js',
