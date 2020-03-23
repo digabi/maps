3fcbae355698434849eb1d92df541f8d757735aa
Sami Saves <sami.saves@reaktor.com>
2020-03-23 17:20:10 +0200

    Make production build smaller

--- webpack.config.js
+++ webpack.config.js
@@ -1,46 +1,47 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 
-module.exports = {
-  entry: './src/index.ts',
-  devtool: 'inline-source-map',
-  module: {
-    rules: [
-      {
-        test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/,
-      },
-      {
-        test: /\.css$/,
-        use: ['style-loader', 'css-loader']
-      },
-      {
-        test: /\.(png|jpe?g|gif)$/i,
-        use: [
-          {
-            loader: 'file-loader',
-          },
-        ],
-      },
-    ],
-  },
-  resolve: {
-    extensions: ['.tsx', '.ts', '.js'],
-  },
-  output: {
-    filename: 'bundle.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      template: path.join(__dirname, 'public', 'index.html'),
-    })
-  ],
-  devServer: {
-    contentBase: path.join(__dirname, 'dist'),
-    compress: true,
-    port: 8080,
-    contentBase: [path.join(__dirname, "maps")]
-  },
-};
+module.exports = (env, argv) => {
+  const isProduction = argv && argv['mode'] === 'production'
+  const plugins = []
+
+  if (!isProduction) {
+    plugins.push(new HtmlWebpackPlugin({ template: path.join(__dirname, 'public', 'index.html') }))
+  }
+
+  return {
+    entry: './src/index.ts',
+    devtool: isProduction ? 'cheap-module-source-map' : 'inline-source-map',
+    module: {
+      rules: [
+        {
+          test: /\.tsx?$/,
+          use: 'ts-loader',
+          exclude: /node_modules/
+        },
+        {
+          test: /\.css$/,
+          use: ['style-loader', 'css-loader']
+        },
+        {
+          test: /\.(png|jpe?g|gif)$/i,
+          use: [{ loader: 'file-loader' }]
+        },
+      ],
+    },
+    resolve: {
+      extensions: ['.tsx', '.ts', '.js'],
+    },
+    output: {
+      filename: 'bundle.js',
+      path: path.resolve(__dirname, 'dist'),
+    },
+    plugins,
+    devServer: {
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 8080,
+      contentBase: [path.join(__dirname, "maps")]
+    },
+  };
+}
