befa89e86b90f6e86d177e734351eca1db187b09
Sami Saves <sami.saves@reaktor.com>
2020-03-23 17:19:37 +0200

    Add map container as createMap parameter

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,21 @@
 import * as leaflet from 'leaflet'
 import 'leaflet/dist/leaflet.css'
 
-const createMap = ({ mapUrl = '/fi/{z}/{x}/{y}.png', minZoom = 0, maxZoom = 9, errorTileUrl = '/error.png' }) => {
-  const map = leaflet.map('map-container').setView([0, 0], 1)
+interface CreateMapParams {
+  container: string | HTMLElement
+  mapUrl: string
+  minZoom: number
+  maxZoom: number
+  errorTileUrl: string
+}
+const createMap = ({
+  container = 'map-container',
+  mapUrl = '/fi/{z}/{x}/{y}.png',
+  minZoom = 0,
+  maxZoom = 9,
+  errorTileUrl = '/error.png'
+}: CreateMapParams) => {
+  const map = leaflet.map(container).setView([0, 0], 1)
 
   const layerOptions: leaflet.TileLayerOptions = {
     minZoom,
