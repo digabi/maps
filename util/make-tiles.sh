19c5e5c3792296e540ae3c9f145bf38129a5cbe0
Sami Saves <sami.saves@reaktor.com>
2020-06-04 14:27:35 +0300

    Finalize maastokartta tile creation script

--- util/make-tiles.sh
+++ util/make-tiles.sh
@@ -1,37 +1,43 @@
 #!/bin/bash
-
 originalFile=$1
-zoomLevel=$2
+destinationPath=$2
+xTiles=$3
+yTiles=$4
 
 rm -rf temp
 mkdir temp
-rm -rf $zoomLevel
-mkdir $zoomLevel
+rm -rf $destinationPath
+mkdir -p $destinationPath
 
+oldSize=$(identify -format "%[fx:w]x%[fx:h]" $originalFile)
 
-width=$(identify -format "%[fx:w]" $originalFile)
-height=$(identify -format "%[fx:h]" $originalFile)
-newWidth=$(expr 256 - $width % 256 + $width)
-newHeight=$(expr 256 - $height % 256 + $height)
+width=$(identify -format "%[fx:w]" $file)
+height=$(identify -format "%[fx:h]" $file)
+newWidth=$(($xTiles * 256))
+newHeight=$(($yTiles * 256))
+newSize="${newWidth}x${newHeight}"
 
-# If image size is not divisible by 256, fill right and bottom edges with gray color until it is
-echo "Adding padding to image. New size will be $newWidth x $newHeight (old size: $width x $height)"
-convert $originalFile -resize "${width}x${height}" -background "#CDCDCD" -gravity NorthWest -extent "${newWidth}x${newHeight}" temp/source.png
+echo "Scaling image from $oldSize to $newSize"
+convert $originalFile -resize $newSize -background "#CDCDCD" -gravity NorthWest -extent $newSize temp/scaled.png
 
-echo "Splitting image into map tiles"
-convert temp/source.png -crop 256x256 \
+echo "Splitting image into map tiles. Total of $xTiles x $yTiles tiles will be created"
+convert temp/scaled.png -crop 256x256 \
   -set filename:tile "%[fx:page.x/256]_%[fx:page.y/256]" \
   +repage "temp/part_%[filename:tile].png"
 
-echo "Orgainizng images"
+echo "Orgainizng images into correct structure ($destinationPath/x/y.png)"
 FILES=temp/part_*.png
 for file in $FILES
 do
   filenameNumbers=$(echo $file | egrep -o [0-9]+)
   folder=$(echo $filenameNumbers | cut --delimiter " " --fields 1)
   filename=$(echo $filenameNumbers | cut --delimiter " " --fields 2)
-  mkdir -p "$zoomLevel/$folder"
-  mv $file "$zoomLevel/$folder/$filename.png"
+  mkdir -p "$destinationPath/$folder"
+  mv $file "$destinationPath/$folder/$filename.png"
 done
 
+echo "Cleaning up"
+
 rm -r temp
+
+echo "Done!"
